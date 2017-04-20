fc51bee9894a828b8814b0ceefd03908afca5bbb
reyesm2 <reyes.marlon57@gmail.com>
2017-04-19 18:56:12 -0600

    1

--- Segunda-Analisis/src/clases/Matriz.java
+++ Segunda-Analisis/src/clases/Matriz.java
@@ -14,10 +14,11 @@ import java.util.Random;
  */
 public class Matriz {
     private Celda[][] matriz;
+    private int tablero;
     
     public Matriz(){};
 
-    public Celda[][] getMatriz() {
+    public Celda[][] getCelda() {
         return matriz;
     }
 
@@ -28,20 +29,18 @@ public class Matriz {
     
     public void crearMatriz(int size){
         matriz = new Celda[size][size];
+        tablero = 0;
         Random  rnd = new Random();
         for(int i=0;i<matriz.length;i++){
             for(int j=0;j<matriz.length;j++){
             Celda celda;
-            int prue = (int)(rnd.nextDouble() * 100+1);
-            
-            if(prue<33){
-                celda = new Celda();
-            }else if(prue<66){
-                celda = new Celda(prue);
+            if(i==0 || i==matriz.length-1 || j==0 || j==matriz.length-1){
+                celda = new Celda(-1);
+                matriz[i][j] = celda;    
             }else{
-                celda = new Celda(10,12);
-            }
-            matriz[i][j] = celda;    
+                celda = new Celda(0);
+                matriz[i][j] = celda;    
+            }      
             }
             
         }
@@ -52,7 +51,8 @@ public class Matriz {
                 if("t1".equals(matriz[i][j].getTipo())){
                     System.out.print("("+matriz[i][j].getValor()+",-1"+") ");
                 }else if("t2".equals(matriz[i][j].getTipo())){
-                    System.out.print("("+matriz[i][j].getValor()+",00"+") ");
+                    //System.out.print(matriz[i][j].getValor());
+                    System.out.print("("+matriz[i][j].getValor()+",0"+") ");
                 }else{
                     System.out.print("("+matriz[i][j].getDerecha()+","+matriz[i][j].getAbajo()+") ");
                 }
@@ -64,6 +64,25 @@ public class Matriz {
             System.out.print(imprimir);
     }
 
+    public int getTamano(){
+        return matriz.length;
+    }
+    
+    public int getDimensiones(){
+        return matriz.length*matriz.length;
+    }
+    
+    public void tablero(){
+        int contador = 0;
+        for(int i=1;i<matriz.length;i++){
+            for(int j=0;j<matriz.length;j++){
+                if(matriz[i][j].getValor() ==-1){
+                    
+                }
+            }
+        }
+    }
+    
     @Override
     public String toString() {
         return "Matriz{" + "matriz=" + matriz + '}';
--- Segunda-Analisis/src/clases/pruebas.java
+++ Segunda-Analisis/src/clases/pruebas.java
@@ -72,6 +72,7 @@ public class pruebas {
         }
         System.out.println(cadena);
     }
+
     
     public static int[] numerosSumados(int[]result,int numero,ArrayList<int[]>resultado){
         imprimir(result);
@@ -93,25 +94,41 @@ public class pruebas {
         }
         return null;
     }
-    public static int[] numerosSumados1(int[]result,int numero,ArrayList<int[]>resultado){
+    
+    public static void imprimirArrayList(ArrayList<int[]>resultado){
+        for(int i=0;i<resultado.size();i++){
+            imprimir(resultado.get(i));
+        }
+    }
+    
+    
+    public static void numerosSumados1(int[]result,int numero,ArrayList<int[]>resultado,int k){
         Random  rnd = new Random();
         //imprimir(result);
+        //System.out.println("k1 "+ k);
         if(suma(result)==numero && ceros(result)==false){
-            imprimir(result);
-            return result;
+            int[] resultAux = (int[])(result.clone());
+            if(resultado.contains(resultAux)==true){
+                System.out.println("Elemento repetido");
+            }else{
+                resultado.add(resultAux);
+                //return numerosSumados1(result,numero,resultado,k++);
+            }
         }
         else{
-            int i = (int)(rnd.nextDouble()*3+0);
-              for(int j=1;j<10;j++){
-                 System.out.println(j);
-                if(existeElemento(result,j)==false && j < numero-1){
-                    result[i] = j;
-                    numerosSumados1(result,numero,resultado);
+            if(k <= result.length){
+                k++;
+                int i = (int)(rnd.nextDouble()*result.length+0);
+                //System.out.println("i :"+i);
+                for(int j=1;j<10;j++){
+                    if(existeElemento(result,j)==false && j < numero-1){
+                        result[i] = j;
+                        //System.out.println("k "+k);
+                        numerosSumados1(result,numero,resultado,k);
+                    }
                 }
-            }
-            System.out.println("For");
+            }   
         }
-        return null;
     }
     
     public static int[] numerosSumadosA(int[]result,int numero,ArrayList<int[]>resultado){;
@@ -130,27 +147,161 @@ public class pruebas {
                     }
                 }
             }
-              
-            //System.out.println("For");
         }
         return null;
     }
     
     
+    public static boolean filaDer(Matriz m,int numero,int fila,int columna){
+        for(int i=columna;i<m.getTamano();i++){
+            Celda celda = m.getCelda()[fila][i];
+            System.out.println(celda.getValor());
+            if("t1".equals(celda.getTipo()) || "t3".equals(celda.getTipo())){
+                return false;
+            }else{
+                if(celda.getValor()==numero){
+                return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    public static boolean filaIzq(Matriz m,int numero,int fila,int columna){
+        for(int i=columna;i>0;i--){
+            Celda celda = m.getCelda()[fila][i];
+            if("t1".equals(celda.getTipo()) || "t3".equals(celda.getTipo())){
+                return false;
+            }else{
+                if(celda.getValor()==numero){
+                return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    public static boolean columnaAba(Matriz m,int numero,int fila,int columna){
+        for(int i=fila;i<m.getTamano();i++){
+            Celda celda = m.getCelda()[i][columna];
+            if("t1".equals(celda.getTipo()) || "t3".equals(celda.getTipo())){
+                return false;
+            }else{
+                if(celda.getValor()==numero){
+                return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    public static boolean columnaArr(Matriz m,int numero,int fila,int columna){
+        for(int i=fila;i>0;i--){
+            Celda celda = m.getCelda()[i][columna];
+            if("t1".equals(celda.getTipo()) || "t3".equals(celda.getTipo())){
+                return false;
+            }else{
+                if(celda.getValor()==numero){
+                return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    public static void backNum(int[]result,int numero,ArrayList<int[]>resultado,int k){
+        int[]resultAux = (int[])(result.clone());
+        if(suma(resultAux)==numero && ceros(resultAux)!=true){
+            resultado.add(resultAux);
+        }else{
+            if(k<result.length){
+                int k1 = k+1;
+                for(int i=1;i<=9;i++){
+                    if(existeElemento(resultAux,i)!=true && i<numero){
+                        resultAux[k]=i;
+                        backNum(resultAux,numero,resultado,k1);
+                    }
+                }
+            }
+        }
+    }
+    
+    public static Matriz backResolver(Matriz m,int tamano,int colocadas){
+        m.crearMatriz(tamano);
+        if(colocadas==m.getTamano()){
+            return m;
+        }else{
+            
+        }
+        return m;
+    }
+    
+    public static Matriz rellenarPared(Matriz m){
+        
+        
+        return m;
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
-        pruebas prue  = new pruebas();
-        int result[] = new int[3];
+//        pruebas prue  = new pruebas();
+        int per = 1; int per1 = 1;
+        int numero = 4;
+        int casillas = 2;
+        int result[] = new int[casillas];
         ArrayList<int[]> resultado = new ArrayList<int[]>();
-        //numerosSumados(result,10,resultado);
-        
-        //result = numerosSumados1(result,10,resultado);
-        Porque(0);
-        if(result!= null){
-            //imprimir(result);
+        if(per ==1){
+            backNum(result,numero,resultado,0);
+            imprimirArrayList(resultado);
+            System.out.println(resultado.size());
         }
-        else{
-            System.out.println("null");
-        }   
+        if(per1 == 1){
+            Matriz m = new Matriz();
+            m.crearMatriz(5);
+            m.getCelda()[3][3].setValor(2);
+            m.imprimirMatriz();
+            if(columnaArr(m,2,4,3)==true){
+                System.out.println("repetido");
+            }
+        }
+        
+//          Matriz m = new Matriz();
+//          backCrear(m,5,0);
+//          m.crearMatriz(14);
+//          m.getCelda()[3][3].setValor(-1);
+//          m.getCelda()[4][4].setValor(-1);
+//          m.getCelda()[5][3].setValor(-1);
+//          m.getCelda()[6][3].setValor(-1);
+//          m.getCelda()[7][3].setValor(-1);
+//          m.getCelda()[8][3].setValor(-1);
+//          m.getCelda()[9][4].setValor(-1);
+//          m.getCelda()[6][1].setValor(-1);
+//          m.getCelda()[6][2].setValor(-1);
+//          m.getCelda()[7][1].setValor(-1);
+//          m.getCelda()[7][2].setValor(-1);
+//          m.getCelda()[10][3].setValor(-1);
+//          m.getCelda()[12][1].setValor(-1);
+//          m.getCelda()[8][5].setValor(-1);
+//          m.getCelda()[1][6].setValor(-1);
+//          m.getCelda()[2][6].setValor(-1);
+//          m.getCelda()[7][6].setValor(-1);
+//          m.getCelda()[6][7].setValor(-1);
+//          m.getCelda()[7][7].setValor(-1);
+//          m.getCelda()[8][8].setValor(-1);
+//          m.getCelda()[9][9].setValor(-1);
+//          m.getCelda()[10][10].setValor(-1);
+//          m.getCelda()[11][7].setValor(-1);
+//          m.getCelda()[12][7].setValor(-1);
+//          m.getCelda()[5][8].setValor(-1);
+//          m.getCelda()[4][9].setValor(-1);
+//          m.getCelda()[3][10].setValor(-1);
+//          m.getCelda()[6][11].setValor(-1);
+//          m.getCelda()[7][11].setValor(-1);
+//          m.getCelda()[6][12].setValor(-1);
+//          m.getCelda()[6][12].setValor(-1);
+//          
+//          //System.out.println(m.getTamano());
+//          m.imprimirMatriz();
+        
     }    
 }
